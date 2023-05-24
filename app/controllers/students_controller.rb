19a963f68ab1baeee18f5c0ba0d8f276f7d9fc3c
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-05-24 17:15:08 -0400

    done including bonus!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
-
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      name = params[:name].titleize
+      students = Student.where(first_name: name).or(Student.where(last_name: name))
+      # byebug
+      render json: students
+    else
+      students = Student.all
+      render json: students
+    end
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -1,64 +1,62 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Students", type: :request do
   before do
-    Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
-    Student.create(first_name: 'Idris', last_name: 'Elba', grade: 105)
-    Student.create(first_name: 'Vanessa', last_name: 'Kirby', grade: 85)
-    Student.create(first_name: 'Jason', last_name: 'Statham', grade: 70)
+    Student.create(first_name: "Dwayne", last_name: "Johnson", grade: 99)
+    Student.create(first_name: "Idris", last_name: "Elba", grade: 105)
+    Student.create(first_name: "Vanessa", last_name: "Kirby", grade: 85)
+    Student.create(first_name: "Jason", last_name: "Statham", grade: 70)
   end
 
   let!(:first_student) { Student.first }
   let!(:second_student) { Student.second }
 
   describe "GET /students/:id" do
-    it 'returns the first student' do
+    it "returns the first student" do
       get "/students/#{first_student.id}"
 
       expect(response.body).to include_json({
         id: a_kind_of(Integer),
-        first_name: 'Dwayne', 
-        last_name: 'Johnson', 
-        grade: 99
+        first_name: "Dwayne",
+        last_name: "Johnson",
+        grade: 99,
       })
     end
 
-    it 'returns the second student' do
+    it "returns the second student" do
       get "/students/#{second_student.id}"
 
       expect(response.body).to include_json({
         id: a_kind_of(Integer),
-        first_name: 'Idris', 
-        last_name: 'Elba', 
-        grade: 105
+        first_name: "Idris",
+        last_name: "Elba",
+        grade: 105,
       })
     end
   end
 
   describe "GET /students" do
-    it 'returns an array of all students' do
-      get '/students'
+    it "returns an array of all students" do
+      get "/students"
 
       expect(response.body).to include_json([
-        { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
-        { first_name: 'Idris', last_name: 'Elba', grade: 105 },
-        { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
-        { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+        { first_name: "Dwayne", last_name: "Johnson", grade: 99 },
+        { first_name: "Idris", last_name: "Elba", grade: 105 },
+        { first_name: "Vanessa", last_name: "Kirby", grade: 85 },
+        { first_name: "Jason", last_name: "Statham", grade: 70 },
       ])
     end
 
-    ## BONUS: un-comment out the code below to run the bonus test
+    # BONUS: un-comment out the code below to run the bonus test
 
-    # context 'with query params' do
-    #   it 'returns students whose first or last name matches the query' do
-    #     get '/students?name=kirby'
+    context "with query params" do
+      it "returns students whose first or last name matches the query" do
+        get "/students?name=kirby"
 
-    #     expect(response.body).to include_json([
-    #       { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
-    #     ])
-    #   end
-    # end
-    
+        expect(response.body).to include_json([
+          { first_name: "Vanessa", last_name: "Kirby", grade: 85 },
+        ])
+      end
+    end
   end
-  
 end
